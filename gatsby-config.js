e7e43427621b33e8bc6f36607d80eeed7a18ca7d
oka0509 <76876781+oka0509@users.noreply.github.com>
2023-03-27 02:40:54 +0900

    目次のプラグインを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,5 +129,24 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents",
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+        ],
+      },
+    },
   ],
 }
